644f5137abf2ca6a647366bbedb8e96d414e65d3
Ammiel <aemlcruz@gmail.com>
2017-06-17 08:47:28 -0400

    added javadoc to scoremodel

--- src/main/java/ca/aemc/rex1/ScoreModel.java
+++ src/main/java/ca/aemc/rex1/ScoreModel.java
@@ -7,6 +7,9 @@ public class ScoreModel
     private long startTime;
     private long elapsedTime;
 
+    /**
+     * Constructor
+     */
     public ScoreModel()
     {
         attempts = 0;
@@ -14,21 +17,41 @@ public class ScoreModel
         startTime = System.currentTimeMillis();
     }
 
+    /**
+     * Returns the number of attemps.
+     *
+     * @return The number attempts.
+     */
     public int getAttempts()
     {
         return attempts;
     }
 
+    /**
+     * Returns the number of successful attempts.
+     *
+     * @return Returns the number of successful attempts.
+     */
     public int getSuccess()
     {
         return successes;
     }
 
+    /**
+     * Returns the start time.
+     *
+     * @return Returns the start time.
+     */
     public long getStart()
     {
         return startTime;
     }
 
+    /**
+     * Returns the difference between the curren time and the start time.
+     *
+     * @return Returns the elapsed time.
+     */
     public long getElapsedTime()
     {
         elapsedTime = System.currentTimeMillis();
@@ -36,23 +59,35 @@ public class ScoreModel
         return difference;
     }
 
+    /**
+     * Increments the number of successes and attempts.
+     *
+     * @param success A boolean variable that indiciates if the user was successful.
+     */
     public void record(boolean success)
     {
         if (success) successes++;
         attempts++;
     }
 
+    /**
+     * Returns the average score by the user.
+     *
+     * @return A double variable calculated from the number of successes and attempts.
+     */
     public double getAverageScore()
     {
         return ((double) successes / attempts) * 100;
     }
 
+    /**
+     * Resets the timer back to zero.
+     */
     public void resetTimer()
     {
         startTime = System.currentTimeMillis();
     }
 
-    // score model test
     public static void main(String[] args)
     {
         ScoreModel sm = new ScoreModel();
